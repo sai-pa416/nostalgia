e6af8b70334b64e01f078793d3a81976b1bf4e17
sai-pa416 <saipratyush.14.2008.13@gmail.com>
2026-08-13 03:01:29 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -10,16 +10,16 @@ export default function Home() {
         <div className="absolute inset-0 bg-gradient-to-b from-black/35 via-transparent to-black/80" />
       </div>
 
-      {/* Fixed film grain */}
+      {}
       <div aria-hidden className="grain fixed inset-0 -z-10 opacity-30 mix-blend-overlay" />
 
-      {/* Floating music notes behind the player */}
+      {}
       <NotesFx />
 
       {/* Fixed top row */}
       <Clock />
 
-      {/* Fixed bottom console: playlist tabs + player */}
+      {}
       <div className="fixed inset-x-0 bottom-0 z-10 flex flex-col items-center px-4 pt-3 pb-[max(1rem,env(safe-area-inset-bottom))]">
         <Player />
       </div>
